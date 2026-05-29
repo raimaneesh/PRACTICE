d7a774345cbeb7ea99f136b9d3f352a88f61dfb2
Maneesh <raimaneesh92@gmail.com>
2026-05-29 18:02:35 +0530

    correct code

--- manu.tf
+++ manu.tf
@@ -1,21 +1,15 @@
-terraform{
-required_providers{
-azurerm ={
-source ="hashicorp/azurerm"
-virsion="4.74.0"
+terraform {
+  required_providers {
+    azurerm = {
+        source ="hashicorp/azurerm"
+        version="4.73.0"
+    }
+  }
 }
-}
-}
-provider"azurerm"{
-features{}
+provider "azurerm"{
+    features{}
 }
 resource"azurerm_resource_group""rg"{
-name ="maneesh"
-location"eastus"
-}
-resource"azurerm_storage_account""sa"{
-name="mm321"
-location="westus"
-account_replication_type ="LRS"
-account_tier ="Standard"
+    name ="maneesh"
+    location ="eastus"
 }
